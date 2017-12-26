0620ea8a04c5cef0d9e8f39f818747b6ab0c060b
akorb <andreas.d.korb@gmail.com>
2017-12-26 20:00:54 +0100

    Not using deprecated method anymore

--- app/src/main/java/com/andy/simplecidgetter/MainTabActivity.java
+++ app/src/main/java/com/andy/simplecidgetter/MainTabActivity.java
@@ -50,7 +50,7 @@ public class MainTabActivity extends Activity implements ActionBar.TabListener {
         // When swiping between different sections, select the corresponding
         // tab. We can also use ActionBar.Tab#select() to do this if we have
         // a reference to the Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+        mViewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
                 actionBar.setSelectedNavigationItem(position);
