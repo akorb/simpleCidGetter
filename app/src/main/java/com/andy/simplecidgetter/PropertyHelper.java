dd42ad49edbae5d2c386d2f958cc5f5bda844f69
akorb <andreas.d.korb@gmail.com>
2018-03-03 17:07:50 +0100

    Reformat

--- app/src/main/java/com/andy/simplecidgetter/PropertyHelper.java
+++ app/src/main/java/com/andy/simplecidgetter/PropertyHelper.java
@@ -4,80 +4,65 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 
-final class PropertyHelper
-{
-	private PropertyHelper()
-	{
-		// Hide constructor
-	}
+final class PropertyHelper {
+    private PropertyHelper() {
+        // Hide constructor
+    }
 
-	public static String getAll()
-	{
-		try
-		{
-			Scanner reader = new Scanner(Runtime.getRuntime().exec("getprop")
-					.getInputStream());
+    public static String getAll() {
+        try {
+            Scanner reader = new Scanner(Runtime.getRuntime().exec("getprop")
+                    .getInputStream());
 
-			StringBuilder sb = new StringBuilder();
-			String separator = System.getProperty("line.separator");
+            StringBuilder sb = new StringBuilder();
+            String separator = System.getProperty("line.separator");
 
-			while (reader.hasNext())
-			{
-				String s1 = reader.nextLine();
+            while (reader.hasNext()) {
+                String s1 = reader.nextLine();
 
-				sb.append(s1);
-				sb.append(separator);
-			}
+                sb.append(s1);
+                sb.append(separator);
+            }
 
-			reader.close();
+            reader.close();
 
-			return sb.toString();
-		}
-		catch (IOException ex)
-		{
-			return "";
-		}
-	}
+            return sb.toString();
+        } catch (IOException ex) {
+            return "";
+        }
+    }
 
-	public static String getPropertyValue(String propertyName)
-	{
-		try
-		{
-			Scanner reader = new Scanner(Runtime.getRuntime()
-					.exec("getprop " + propertyName).getInputStream());
+    public static String getPropertyValue(String propertyName) {
+        try {
+            Scanner reader = new Scanner(Runtime.getRuntime()
+                    .exec("getprop " + propertyName).getInputStream());
 
-			String result;
+            String result;
 
-			if (reader.hasNext())
-			{
-				result = reader.nextLine();
-			}
-			else
-			{
-				result = "";
-			}
+            if (reader.hasNext()) {
+                result = reader.nextLine();
+            } else {
+                result = "";
+            }
 
-			reader.close();
-			return result;
-		}
-		catch (IOException ex)
-		{
-			return "";
-		}
-	}
+            reader.close();
+            return result;
+        } catch (IOException ex) {
+            return "";
+        }
+    }
 
-	public static String getCid()
-	{
-		// The cid is stored usually stored in ro.cid
-		String primaryCid = getPropertyValue("ro.cid");
-		if (!primaryCid.isEmpty())
-			return primaryCid.toUpperCase(Locale.getDefault());
-			// ro.cid does not exist for each device.
-		// I guess it's for GPE devices, but this isn't for sure yet.
-		String fallbackCid = getPropertyValue("ro.boot.cid");
-		if (!fallbackCid.isEmpty())
-			return fallbackCid.toUpperCase(Locale.getDefault());
-		// When cid does not exist, return empty string.
-		return "";
-	}
+    public static String getCid() {
+        // The cid is stored usually stored in ro.cid
+        String primaryCid = getPropertyValue("ro.cid");
+        if (!primaryCid.isEmpty())
+            return primaryCid.toUpperCase(Locale.getDefault());
+        // ro.cid does not exist for each device.
+        // I guess it's for GPE devices, but this isn't for sure yet.
+        String fallbackCid = getPropertyValue("ro.boot.cid");
+        if (!fallbackCid.isEmpty())
+            return fallbackCid.toUpperCase(Locale.getDefault());
+        // When cid does not exist, return empty string.
+        return "";
+    }
 }
