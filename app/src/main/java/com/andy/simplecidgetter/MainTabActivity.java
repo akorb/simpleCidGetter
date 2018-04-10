fb01970b6e2b1a372895311275a7f1f03988914c
akorb <andreas.d.korb@gmail.com>
2018-04-10 15:32:09 +0200

    Updated versions

--- app/src/main/java/com/andy/simplecidgetter/MainTabActivity.java
+++ app/src/main/java/com/andy/simplecidgetter/MainTabActivity.java
@@ -81,9 +81,9 @@ public class MainTabActivity extends AppCompatActivity {
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
