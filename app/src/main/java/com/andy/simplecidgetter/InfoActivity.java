ee621e774c7a7983f31d2c92e91eade5240c0bb9
akorb <andreas.d.korb@gmail.com>
2018-09-21 22:28:18 +0200

    Updated to Androidx and SDK 28

--- app/src/main/java/com/andy/simplecidgetter/InfoActivity.java
+++ app/src/main/java/com/andy/simplecidgetter/InfoActivity.java
@@ -1,7 +1,7 @@
 package com.andy.simplecidgetter;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoActivity extends AppCompatActivity {
 
--- app/src/main/java/com/andy/simplecidgetter/MainTabActivity.java
+++ app/src/main/java/com/andy/simplecidgetter/MainTabActivity.java
@@ -1,25 +1,26 @@
 package com.andy.simplecidgetter;
 
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v4.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainTabActivity extends AppCompatActivity {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * The {@link androidx.viewpager.widget.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every
      * loaded fragment in memory. If this becomes too memory intensive, it
      * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     * {@link androidx.fragment.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
