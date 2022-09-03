55160246aadef71fcffcd72aa89893eb01a51bbe
Andreas Korb <andreas.d.korb@gmail.com>
2022-09-03 13:07:08 +0200

    Annotate Menu with @NonNull

--- app/src/main/java/com/andy/simplecidgetter/MainTabActivity.java
+++ app/src/main/java/com/andy/simplecidgetter/MainTabActivity.java
@@ -59,7 +59,7 @@ public class MainTabActivity extends AppCompatActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
